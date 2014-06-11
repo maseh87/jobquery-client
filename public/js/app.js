173915ecca92770225168fcbcfa318dca746c26b
Evan Kim <evan.kim.390@gmail.com>
2014-06-11 16:30:58 -0700

    Add array in app.config service injection to prevent future minification issues

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,8 @@
 var app = angular.module('jobQuery', ['ui.router']);
 
-app.config(function($httpProvider) {
+app.config(['$httpProvider', function($httpProvider) {
   $httpProvider.defaults.useXDomain = true; //Enable cross domain calls
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
   // Remove the header used to identify ajax call that would prevent CORS from working
   // http://thibaultdenizet.com/tutorial/cors-with-angular-js-and-sinatra/
-});
+}]);
