ca7453600f64bb43097f99dfa31c0ad4894d39fe
Evan Kim <evan.kim.390@gmail.com>
2014-06-11 15:45:48 -0700

    Restructure js and test folders

--- public/js/admin/subroutes/opportunities/state.js
+++ public/js/admin/subroutes/opportunities/state.js
@@ -3,7 +3,7 @@ app.config(['$stateProvider', function($stateProvider){
   $stateProvider
     .state('admin.opportunities', {
       url: '/opportunities',
-      templateUrl: '/js/admin/opportunities/core/templates/opportunities.tpl.html',
+      templateUrl: '/js/admin/subroutes/opportunities/templates/opportunities.tpl.html',
       controller: 'AdminOpportunitiesCtrl'
     });
 
