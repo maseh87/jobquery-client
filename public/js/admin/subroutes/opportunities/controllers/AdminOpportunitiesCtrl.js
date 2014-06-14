5c1eb70e87f71ac23aa4b421d6656ee2234677f3
willwclo <willwclo@gmail.com>
2014-06-14 16:29:44 -0700

    Experimenting with matches

--- public/js/admin/subroutes/opportunities/controllers/AdminOpportunitiesCtrl.js
+++ public/js/admin/subroutes/opportunities/controllers/AdminOpportunitiesCtrl.js
@@ -1,3 +1,18 @@
-app.controller('AdminOpportunitiesCtrl', ['$scope', function($scope){
+app.controller('AdminOpportunitiesCtrl', ['$scope', '$controller', 'Opportunity', 'Match', 
+  function($scope, $controller, Opportunity, Match) {
+
+  $controller('ResourceCtrl', {$scope: $scope, Resource: Opportunity});
+  $controller('MatchCtrl', {$scope: $scope, Match: Match});
+
+  var compiled = {};
+  $scope.$watch('matches', function(matches) {
+    if (!matches) return null;
+    console.log(matches.data);
+  });
+
+  $scope.test = function() {
+    console.log("Logging scope...");
+    console.log($scope);
+  };
 
 }]);
--- public/js/admin/subroutes/opportunities/state.js
+++ public/js/admin/subroutes/opportunities/state.js
@@ -11,20 +11,19 @@ app.config(['$stateProvider', function($stateProvider){
         }]
       }
     })
-      .state('admin.opportunities.all', {
-        url: '',
-        templateUrl: '/js/admin/subroutes/opportunities/templates/opportunities.tpl.html',
-        controller: 'ResourceCtrl'
-      })
-      .state('admin.opportunities.new', {
-        url: '/new',
-        templateUrl: '/js/admin/subroutes/opportunities/templates/new.tpl.html',
-        controller: 'ResourceNewCtrl'
-      })
-      .state('admin.opportunities.detail', {
-        url: '/:_id',
-        templateUrl: '/js/admin/subroutes/opportunities/templates/detail.tpl.html',
-        controller: 'ResourceDetailCtrl'
-      })
-
+    .state('admin.opportunities.all', {
+      url: '',
+      templateUrl: '/js/admin/subroutes/opportunities/templates/opportunities.tpl.html',
+      controller: 'AdminOpportunitiesCtrl'
+    })
+    .state('admin.opportunities.new', {
+      url: '/new',
+      templateUrl: '/js/admin/subroutes/opportunities/templates/new.tpl.html',
+      controller: 'AdminOpportunitiesDetailCtrl'
+    })
+    .state('admin.opportunities.detail', {
+      url: '/:_id',
+      templateUrl: '/js/admin/subroutes/opportunities/templates/detail.tpl.html',
+      controller: 'AdminOpportunitiesNewCtrl'
+    });
 }]);
