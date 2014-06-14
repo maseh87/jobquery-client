f7a7691a8c7c703e97b5bd62b504f1b6994971d7
willwclo <willwclo@gmail.com>
2014-06-14 16:41:49 -0700

    Complete opp list

--- public/js/admin/subroutes/opportunities/controllers/AdminOpportunitiesCtrl.js
+++ public/js/admin/subroutes/opportunities/controllers/AdminOpportunitiesCtrl.js
@@ -4,10 +4,19 @@ app.controller('AdminOpportunitiesCtrl', ['$scope', '$controller', 'Opportunity'
   $controller('ResourceCtrl', {$scope: $scope, Resource: Opportunity});
   $controller('MatchCtrl', {$scope: $scope, Match: Match});
 
-  var compiled = {};
+  var interest = {};
   $scope.$watch('matches', function(matches) {
     if (!matches) return null;
-    console.log(matches.data);
+    if (!$scope.resources) return null;
+    matches.data.forEach(function (match) {
+      if (!interest[match.oppId]) { interest[match.oppId] = 0; }
+      if (match.userInterest >= 3) { interest[match.oppId]++; }
+    });
+    $scope.resources.forEach(function (opportunity) {
+      opportunity.userInterest = interest[opportunity._id];
+      var link = "www." + opportunity.company.name.split(' ')[0] + ".ca"
+      opportunity.links = [link];
+    });
   });
 
   $scope.test = function() {
