c1da59ee3845a6ca7621b7f70801f2d37a1d5ef9
Evan Kim <evan.kim.390@gmail.com>
2014-06-15 16:25:08 -0700

    Refactor Company restful service

--- public/js/admin/subroutes/companies/controllers/AdminCompaniesDetailCtrl.js
+++ public/js/admin/subroutes/companies/controllers/AdminCompaniesDetailCtrl.js
@@ -1,5 +1,7 @@
-app.controller('AdminCompaniesDetailCtrl', ['$scope', '$controller', 'Resource', function($scope, $controller, Resource){
-  $controller('ResourceDetailCtrl', {$scope: $scope, Resource: Resource});
-  $controller('ResourceEditCtrl', {$scope: $scope, Resource: Resource});
+app.controller('AdminCompaniesDetailCtrl', ['$scope', 'Company', '$stateParams', function($scope, Company, $stateParams){
+
+  Company.get($stateParams._id).then(function(company){
+    $scope.company = company;
+  });
 
 }]);
