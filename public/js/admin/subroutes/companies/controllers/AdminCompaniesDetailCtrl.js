ed6c0afb6d76adea5a09a657027942a4beb81fa5
Evan Kim <evan.kim.390@gmail.com>
2014-06-16 16:47:39 -0700

    Add update functionality on admin.companies.detail page

--- public/js/admin/subroutes/companies/controllers/AdminCompaniesDetailCtrl.js
+++ public/js/admin/subroutes/companies/controllers/AdminCompaniesDetailCtrl.js
@@ -5,4 +5,37 @@ app.controller('AdminCompaniesDetailCtrl', ['$scope', 'Company', '$stateParams',
     $scope.opportunities = company.opportunities;
   });
 
+  var removeEmptyFields = function(){
+    $scope.company.media = $scope.company.media.filter(function(item){
+      return item.text;
+    });
+
+    $scope.company.links = $scope.company.links.filter(function(item){
+      return item.text;
+    });
+  };
+
+  $scope.update = function(){
+    removeEmptyFields();
+    Company.update($scope.company).then(function(data){
+      console.log('Update successful');
+    });
+  };
+
+  $scope.addMedia = function(){
+    $scope.company.media.push({text: ''});
+  };
+
+  $scope.removeMedia = function(index){
+    $scope.company.media.splice(index, 1);
+  };
+
+  $scope.addLink = function(){
+    $scope.company.links.push({text: ''});
+  };
+
+  $scope.removeLink = function(index){
+    $scope.company.links.splice(index, 1);
+  };
+
 }]);
