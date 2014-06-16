506b4082e12712a228b31cfec2325e2eebaf5aa2
willwclo <willwclo@gmail.com>
2014-06-16 15:17:41 -0700

    Change mock data for opportunities to include more details

--- public/mocks/opportunities.js
+++ public/mocks/opportunities.js
@@ -7,11 +7,37 @@ for(var i = 1; i < 50; i++){
     active: true,
     jobTitle: faker.random.bs_buzz(),
     description: faker.Lorem.sentence(),
-    tags: [],
-    links: [],
+    tags: [
+        {
+            tagId: {
+                name: faker.random.bs_buzz(),
+                label: '',
+                scaleDescription: [],
+                isPublic: true,
+                category: '',
+            }, 
+            value: 4
+        }, {
+            tagId: {
+                name: faker.random.bs_buzz(),
+                label: '',
+                scaleDescription: [],
+                isPublic: true,
+                category: '',
+            },
+            value: 3
+        }
+    ],
+    links: [
+        {title: 'Blog', url: 'www.google.ca/blog'},
+        {title: 'Jobs', url: 'www.google.ca/jobs'}
+    ],
     notes: [],
     internalNotes: [],
-    questions: [],
+    questions: [
+        {date: 1402946938842, text: 'A question?'},
+        {date: 1400956946058, text: 'Another one?'}
+    ],
     survey: [],
     users: [],
     company: companies[Math.floor(Math.random() * 50)],
