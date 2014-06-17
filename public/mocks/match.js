df55449fefb21c98678383ac34bc6f8b924a70cd
Evan Kim <evan.kim.390@gmail.com>
2014-06-16 21:48:44 -0700

    Fix mocks/match.js to not repeat match combinations

--- public/mocks/match.js
+++ public/mocks/match.js
@@ -1,20 +1,25 @@
 var matches = [];
 
 for(var i = 1; i < 50; i++){
-  for(var j = 0; j < 25; j++){
-    var date = faker.Date.recent(5);
-    var usedIds = {};
+  var usedIds = {};
+  var date = faker.Date.recent(5);
+  for(var j = 0; j < 30; j++){
     var index = Math.floor((Math.random() * 50) + 1);
-    if(!usedIds[index]){
-      usedIds[index] = true;
-      matches.push({
-        userId: i,
-        oppId: index,
-        userInterest: Math.floor((Math.random() * 4) + 1),
-        answers: [],
-        createdAt: date,
-        updatedAt: date 
-      });
-    }
+    usedIds[index] = true;
+  }
+
+  var oppIds = Object.keys(usedIds);
+  console.log(oppIds);
+  for(var j = 0; j < oppIds.length; j++){
+    matches.push({
+      userId: i,
+      oppId: parseInt(oppIds[j]),
+      userInterest: Math.floor((Math.random() * 4) + 1),
+      answers: [],
+      createdAt: date,
+      updatedAt: date
+    });
   }
 }
+
+console.log(matches);
