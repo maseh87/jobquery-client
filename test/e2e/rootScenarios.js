2f2e2e3715882a2f77746b0898a49aab4273792f
Evan Kim <evan.kim.390@gmail.com>
2014-06-17 13:31:08 -0700

    Add sidebar + e2e testing

--- test/e2e/rootScenarios.js
+++ test/e2e/rootScenarios.js
@@ -30,4 +30,26 @@ describe('jobQuery application', function(){
 
   });
 
+  describe('sidebar', function(){
+
+    it('should allow you to navigate between states', function(){
+      browser.get('/users');
+      browser.findElement(by.css("a[href*='/users/account']")).click();
+      browser.getLocationAbsUrl().then(function(url){
+        expect(url).toBe('http://localhost:8000/users/account');
+      });
+
+      browser.findElement(by.css("a[href*='/users/companies']")).click();
+      browser.getLocationAbsUrl().then(function(url){
+        expect(url).toBe('http://localhost:8000/users/companies');
+      });
+
+      browser.findElement(by.css("a[href*='/users/opportunities']")).click();
+      browser.getLocationAbsUrl().then(function(url){
+        expect(url).toBe('http://localhost:8000/users/opportunities');
+      });
+    });
+
+  });
+
 });
