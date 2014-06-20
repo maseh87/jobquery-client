2754fccdfabd4f0bd0aadc7582c1aff2c9c8677c
Evan Kim <evan.kim.390@gmail.com>
2014-06-20 11:26:13 -0700

    Add Company Model for Admin portal

---
+++ public/js/admin/services/CategoryModel.js
@@ -0,0 +1,43 @@
+app.factory('Category', ['$http', 'SERVER_URL', function($http, SERVER_URL){
+    var categoryMethods = {};
+
+    categoryMethods.getAll = function(type){
+      if(!type){
+        return $http({
+          method: 'GET',
+          url: SERVER_URL + '/api/categories'
+        }).then(function(response){
+          return response.data;
+        });
+      } else {
+        return $http({
+          method: 'GET',
+          url: SERVER_URL + '/api/categories/type/' + type
+        }).then(function(response){
+          return response.data;
+        });
+      }
+    };
+
+    categoryMethods.create = function(category){
+      return $http({
+        method: 'POST',
+        url: SERVER_URL + '/api/categories',
+        data: category
+      }).then(function(response){
+        return response.data;
+      });
+    };
+
+    categoryMethods.update = function(category){
+      return $http({
+        method: 'PUT',
+        url: SERVER_URL + '/api/categories' + category._id,
+        data: category
+      }).then(function(response){
+        return response.data;
+      });
+    };
+
+    return categoryMethods;
+  }]);
