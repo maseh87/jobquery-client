a8b80fe3cbefe3d87b87d17a845b70818c1d85da
Evan Kim <evan.kim.390@gmail.com>
2014-06-19 22:42:04 -0700

    Add basic handling for survey on user account page

--- public/js/users/subroutes/account/controllers/UsersAccountCtrl.js
+++ public/js/users/subroutes/account/controllers/UsersAccountCtrl.js
@@ -1,10 +1,18 @@
-app.controller('UsersAccountCtrl', ['$scope', 'UsersAccount', function($scope, UsersAccount){
+app.controller('UsersAccountCtrl', ['$scope', 'UsersAccount', 'Tag', function($scope, UsersAccount, Tag){
 
   UsersAccount.get().then(function(user){
     $scope.user = user;
   });
 
+  Tag.getAll().then(function(tags){
+    $scope.binary = tags.filter(function(item){ return item.type === 'binary' });
+    $scope.scale = tags.filter(function(item){ return item.type === 'scale' });
+    $scope.text = tags.filter(function(item){ return item.type === 'text' });
+  });
+
   $scope.update = function(){
+    $scope.user.tags = [];
+    $scope.user.tags.concat($scope.binary, $scope.scale, $scope.text);
     UsersAccount.update($scope.user).then(function(response){
       console.log('User account information updated successfully');
     });
