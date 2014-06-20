91f944240a1d6272a86d8215cc2ac746b99a82a1
Conor Fennell <fennell.conor@gmail.com>
2014-06-19 18:16:33 -0700

    Added interest declared and interest count

--- public/js/admin/subroutes/candidates/controllers/AdminCandidatesCtrl.js
+++ public/js/admin/subroutes/candidates/controllers/AdminCandidatesCtrl.js
@@ -1,7 +1,36 @@
-app.controller('AdminCandidatesCtrl', ['User', '$scope', function(User, $scope){
+app.controller('AdminCandidatesCtrl', ['User', 'Match', '$scope', function(User, Match, $scope){
 
   User.getAll().then(function(users){
     $scope.users = users;
+    var groups = {};
+    var userMap = {};
+    // split users into groups<key,user>
+    users.forEach(function(user) {
+      if(!groups[user.category.name]) {
+        groups[user.category.name] = [];
+      }
+      groups[user.category.name].push(user);
+    });
+
+    users.forEach(function(user) {
+      userMap[user._id] = user;
+      userMap[user._id].interestDeclared = 0;
+      userMap[user._id].interestThreeOrGreater = 0;
+    });
+
+    Match.getAll().then(function(matches){
+      for(var i = 0; i < matches.length; i++) {
+        if (matches[i].userInterest !== 0) {
+          userMap[matches[i].user._id].interestDeclared++;
+          if (matches.userInterest >= 3) {
+            userMap[user._id].interestThreeOrGreater++;
+          }
+        }
+      }
+      $scope.groups = groups;
+    });
+
   });
+
 
 }]);
