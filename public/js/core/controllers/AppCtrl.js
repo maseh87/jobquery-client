abcd4fa8bd428eff464e698fe798fc533ca88434
willwclo <willwclo@gmail.com>
2014-06-20 19:41:11 -0700

    Sidebar styling complete

--- public/js/core/controllers/AppCtrl.js
+++ public/js/core/controllers/AppCtrl.js
@@ -1,4 +1,8 @@
-app.controller('AppCtrl', function($scope) {
+app.controller('AppCtrl', function($scope, $location) {
 
-
+    // a bit hacky but it will do for now!
+    $scope._getCurrentView = function(name) {
+        console.log($location.$$path.split('/'));
+        return $location.$$path.split('/').indexOf(name) >= 0;
+    };
 });
