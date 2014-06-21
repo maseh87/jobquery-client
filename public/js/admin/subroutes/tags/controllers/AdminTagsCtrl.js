a85f5e47942b9320733b6ee7c8239d08fd23c40f
Evan Kim <evan.kim.390@gmail.com>
2014-06-20 21:30:32 -0700

    Implement tag removal/addition and shifting within categories

--- public/js/admin/subroutes/tags/controllers/AdminTagsCtrl.js
+++ public/js/admin/subroutes/tags/controllers/AdminTagsCtrl.js
@@ -1,9 +1,101 @@
-app.controller('AdminTagsCtrl', ['$scope', 'Tag', function($scope, Tag){
+app.controller('AdminTagsCtrl', ['$scope', 'Tag', 'Category', '$q', function($scope, Tag, Category, $q){
 
   var initialize = function(){
+    //Step 1. Fetch all tags
     Tag.getAll().then(function(tags){
-      $scope.tags = tags.filter(function(tag){ return tag.active; });
+      tags = tags.filter(function(tag){ return tag.active; });
+
+      //Step 2. Fetch all categories
+      Category.getAll('Tag').then(function(data){
+
+        //Step 3. Objectify categories, keyed by category._id
+        var categories = {};
+        data.forEach(function(category){
+          category.tags = [];
+          categories[category._id] = category;
+        });
+        categories.uncategorized = {
+          name: 'uncategorized',
+          tags: []
+        };
+        //Step 4. Push tags into categories if one exists. If not, push the tag into undefined.
+        tags.forEach(function(tag){
+          //Set tag.readOnly value for template
+          tag.readOnly = true;
+          //If tag.category is null, push to uncategorized straightaway
+          if(tag.category === null){
+            categories.uncategorized.tags[tag.position] = tag;
+          } else {
+            var id = tag.category._id;
+            if(categories[id]){
+              categories[id].tags[tag.position] = tag;
+            } else {
+              categories.uncategorized.tags[tag.position] = tag;
+            }
+          }
+        });
+
+        //Step 5. Resolve Descrepancies
+        for(var key in categories){
+          var array = categories[key].tags;
+          array.sort(function(a, b){
+            return a - b;
+          });
+          var promises = [];
+          for(var i = 0; i < array.length; i++){
+            var tag = array[i];
+            if(tag === undefined){
+              console.log('undefined tag, splicing rest of array');
+              array.splice(i, array.length - i);
+              break;
+            }
+            if(tag && (tag.position !== i)){
+              console.log(tag);
+              console.log(tag.position, i);
+              tag.position = i;
+              console.log('error found, correcting');
+              promises.push(Tag.update(tag));
+            }
+          }
+          $q.all(promises).then(function(data){
+            $scope.categories = categories;
+          });
+        };
+
+      });
     });
+  };
+
+  $scope.moveUp = function(index, category){
+    //1. Grab current tag.
+    var current = category.tags[index];
+    //2. Grab next tag.
+    var next = category.tags[index - 1];
+    //3. Update positions of both tags.
+    current.position--;
+    next.position++;
+    category.tags.splice(index - 1, 2, current, next);
+    //4. If either tag is saved in the database, submit a PUT request
+    var promises = [];
+    if(current._id) promises.push(Tag.update(current));
+    if(next._id) promises.push(Tag.update(next));
+    return $q.all(promises);
+  };
+
+  $scope.moveDown = function(index, category){
+    //1. Grab current tag.
+    var current = category.tags[index];
+    //2. Grab next tag.
+    var next = category.tags[index + 1];
+    //3. Update positions of both tags.
+    current.position++;
+    next.position--;
+    category.tags.splice(index, 2, next, current);
+    //4. If either tag is saved in the database, submit a PUT request
+    var promises = [];
+    if(current._id) promises.push(Tag.update(current));
+    if(next._id) promises.push(Tag.update(next));
+    return $q.all(promises);
   };
 
   $scope.save = function(tag){
@@ -13,25 +105,64 @@ app.controller('AdminTagsCtrl', ['$scope', 'Tag', function($scope, Tag){
       });
     } else {
       Tag.create(tag).then(function(data){
+        tag._id = data._id;
         console.log('Tag created successfully');
       });
     }
   };
 
-  $scope.add = function(){
-    $scope.tags.unshift({});
+  $scope.add = function(category){
+    category.tags.push({
+      category: category._id,
+      position: category.tags.length
+    });
   };
 
-  $scope.remove = function(tag, index){
-    if(tag._id){
-      tag.active = false;
-      Tag.update(tag).then(function(data){
-        console.log('Tag removed successfully');
-        $scope.tags.splice(index, 1);
-      });
-    } else {
-      $scope.tags.splice(index, 1);
-    }
+  var reduceAfterIndex = function(tags, index){
+    tag.position--;
+    return Tag.update(tag);
+  };
+
+  var increaseTagPosition = function(tag){
+    tag.position++;
+    return Tag.update(tag);
+  };
+
+  $scope.remove = function(tag, index, category){
+
+    var removeTagFromDatabasePromise = function(){
+      if(tag._id){
+        tag.active = false;
+        tag.category = tag.category._id;
+        return Tag.update(tag);
+      } else {
+        var deferred = $q.defer();
+        deferred.resolve();
+        return deferred.promise;
+      }
+    };
+
+    var removeTagFromPage = function(){
+      category.tags.splice(index, 1);
+    };
+
+    var reducePositionOfTagsPromise = function(){
+      var promises = [];
+      for(var i = index; i < category.tags.length; i++){
+        var tag = category.tags[i];
+        tag.position--;
+        if(category.tags[i]._id){
+          promises.push(Tag.update(category.tags[i]));
+        }
+      }
+      return $q.all(promises);
+    };
+
+    removeTagFromDatabasePromise().then(function(){
+      removeTagFromPage();
+      return reducePositionOfTagsPromise();
+    });
+
   };
 
   initialize();
