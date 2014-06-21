9dea8a5d067541eb82eb3273c1c0b2b9332003ab
willwclo <willwclo@gmail.com>
2014-06-20 19:44:09 -0700

    Fix app ctrl

--- public/js/core/controllers/AppCtrl.js
+++ public/js/core/controllers/AppCtrl.js
@@ -1,8 +1,3 @@
 app.controller('AppCtrl', function($scope, $location) {
 
-    // a bit hacky but it will do for now!
-    $scope._getCurrentView = function(name) {
-        console.log($location.$$path.split('/'));
-        return $location.$$path.split('/').indexOf(name) >= 0;
-    };
 });
