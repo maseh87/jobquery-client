49379e3621e5f4f5f2ee1846f4a99072e923cdb4
Jeffrey Gho <jeffreygho@gmail.com>
2014-06-22 12:38:31 -0700

    User Opportunity

--- public/js/users/services/UserOpportunityModel.js
+++ public/js/users/services/UserOpportunityModel.js
@@ -1,19 +1,18 @@
-app.factory('UserOpportunityResource', ['$resource', 'SERVER_URL', function($resource, SERVER_URL){
+app.factory('UserOpportunityResource',
+  ['$resource', 'SERVER_URL', function($resource, SERVER_URL) {
   return $resource(SERVER_URL + '/public/opportunities/:_id', null, {update: {method: 'PUT'}});
 }]);
 
-app.factory('UsersOpportunity', ['UserOpportunityResource', function(UserOpportunityResource){
+app.factory('UsersOpportunity',
+  ['$http', 'SERVER_URL', 'UserOpportunityResource',
+  function($http, SERVER_URL, UserOpportunityResource) {
   var userOpportunityMethods = {};
 
   userOpportunityMethods.getAll = function(){
-    return UserOpportunityResource.query().$promise;
+    return $http.get(SERVER_URL + '/public/opportunities');
   };
 
-  userOpportunityMethods.get = function(id){
-    return UserOpportunityResource.get({_id: id}).$promise;
-  };
-
-  userOpportunityMethods.update = function(opportunity){
+  userOpportunityMethods.update = function(opportunity) {
     return UserOpportunityResource.update({_id: opportunity._id}, opportunity.match).$promise;
   };
 
