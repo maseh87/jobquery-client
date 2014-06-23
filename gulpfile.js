4b3acb2f194ff7e5c13f51c5fdd4f4abbe5381ab
Conor Fennell <fennell.conor@gmail.com>
2014-06-23 14:52:58 -0700

    gulp concat working

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,50 @@
 var gulp = require('gulp');
 
 var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var nodemon = require('gulp-nodemon');
 var processhtml = require('gulp-processhtml');
 
-gulp.task('lint', function(){
+gulp.task('lint', function () {
   return gulp.src('public/js/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('nodemon', function(){
+gulp.task('minify', function () {
+  return gulp.src('public/js/**/*.js')
+    .pipe(concat('jobquery.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/assets/js/'));
+});
+
+gulp.task('html', function () {
+  return gulp.src('public/assets/index.html')
+    .pipe(processhtml('index.html'))
+    .pipe(gulp.dest('public/'));
+});
+
+gulp.task('html-dev', function () {
+  return gulp.src('public/assets/index.html')
+    .pipe(gulp.dest('public/'));
+});
+
+gulp.task('concatbower', function () {
+  return gulp.src([
+    // 'public/bower_components/jquery/dist/jquery.min.js',
+    // 'public/bower_components/bootstrap/dist/js/bootstrap.min.js',
+    'public/bower_components/angular/angular.min.js',
+    'public/bower_components/angular-ui-router/release/angular-ui-router.min.js',
+    'public/bower_components/angular-resource/angular-resource.min.js',
+    'public/bower_components/angular-local-storage/angular-local-storage.min.js',
+    'public/assets/js/jobquery.min.js'],
+    {  base: 'public/'  })
+      .pipe(concat('jobquery.master.min.js'))
+      .pipe(gulp.dest('public/assets/js/'));
+});
+
+gulp.task('nodemon', function () {
   return nodemon({
     script: 'server.js',
     ext: 'js',
@@ -22,8 +56,6 @@ gulp.task('nodemon', function(){
   });
 });
 
-gulp.task('processhtml', function(){
+gulp.task('prod',['minify','html']);
 
-});
-
-gulp.task('default', ['nodemon', 'processhtml']);
+gulp.task('dev',['html-dev']);
