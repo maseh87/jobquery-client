435485c32ffa4b859a1eb8cc359adf6dbd6a0f05
Conor Fennell <fennell.conor@gmail.com>
2014-06-23 17:01:00 -0700

    gulp build process

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var nodemon = require('gulp-nodemon');
+var rename = require('gulp-rename');
+var gulpIgnore = require('gulp-ignore');
 var processhtml = require('gulp-processhtml');
 
 gulp.task('lint', function () {
@@ -12,50 +13,39 @@ gulp.task('lint', function () {
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('minify', function () {
-  return gulp.src('public/js/**/*.js')
+gulp.task('minify-prod', function () {
+  return gulp.src(['public/js/**/*.js', '!public/js/appdev.js'])
     .pipe(concat('jobquery.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('public/assets/js/'));
+    .pipe(gulp.dest('public/lib/'));
 });
 
-gulp.task('html', function () {
-  return gulp.src('public/assets/index.html')
+gulp.task('html-prod', function () {
+  return gulp.src('public/dev.html')
     .pipe(processhtml('index.html'))
     .pipe(gulp.dest('public/'));
 });
 
 gulp.task('html-dev', function () {
-  return gulp.src('public/assets/index.html')
+  return gulp.src('public/dev.html')
+    .pipe(rename('index.html'))
     .pipe(gulp.dest('public/'));
 });
 
-gulp.task('concatbower', function () {
+gulp.task('concatbower-prod',['minify-prod'], function () {
   return gulp.src([
-    // 'public/bower_components/jquery/dist/jquery.min.js',
-    // 'public/bower_components/bootstrap/dist/js/bootstrap.min.js',
+    'public/bower_components/jquery/dist/jquery.min.js',
+    'public/bower_components/bootstrap/dist/js/bootstrap.min.js',
     'public/bower_components/angular/angular.min.js',
     'public/bower_components/angular-ui-router/release/angular-ui-router.min.js',
     'public/bower_components/angular-resource/angular-resource.min.js',
     'public/bower_components/angular-local-storage/angular-local-storage.min.js',
-    'public/assets/js/jobquery.min.js'],
+    'public/lib/jobquery.min.js'],
     {  base: 'public/'  })
       .pipe(concat('jobquery.master.min.js'))
-      .pipe(gulp.dest('public/assets/js/'));
+      .pipe(gulp.dest('public/lib/'));
 });
 
-gulp.task('nodemon', function () {
-  return nodemon({
-    script: 'server.js',
-    ext: 'js',
-    ignore: ['test/']
-  })
-  .on('change', ['lint'])
-  .on('restart', function(){
-    console.log('######## Change Detected, Restarting Server ########');
-  });
-});
-
-gulp.task('prod',['minify','html']);
+gulp.task('prod',['concatbower-prod','html-prod']);
 
 gulp.task('dev',['html-dev']);
