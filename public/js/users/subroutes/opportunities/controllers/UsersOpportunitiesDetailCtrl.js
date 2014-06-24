3794b25d9174cf7686c722cd8274f05653ed2fa2
Evan Kim <evan.kim.390@gmail.com>
2014-06-23 21:46:08 -0700

    Complete basic implementation of user opportunity detail view

--- public/js/users/subroutes/opportunities/controllers/UsersOpportunitiesDetailCtrl.js
+++ public/js/users/subroutes/opportunities/controllers/UsersOpportunitiesDetailCtrl.js
@@ -2,7 +2,22 @@ app.controller('UsersOpportunitiesDetailCtrl', ['$scope', 'UsersOpportunity', '$
 function($scope, UsersOpportunity, $stateParams) {
 
   UsersOpportunity.get($stateParams._id).then(function(data){
-    console.log(data);
+    var match = data.match;
+    var opportunity = match.opportunity;
+
+    $scope.match = match;
+    $scope.opportunity = opportunity;
+    $scope.userInterest = match.userInterest;
+    $scope.questions = match.opportunity.questions;
+    $scope.answers = match.answers;
   });
+
+  $scope.submit = function(){
+    var match = $scope.match;
+    match.answers = $scope.answers;
+    match.userInterest = parseInt($scope.match.userInterest);
+    console.log(match);
+    UsersOpportunity.update(match);
+  };
 
 }]);
