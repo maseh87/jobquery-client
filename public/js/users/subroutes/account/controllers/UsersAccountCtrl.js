3624b071630df0dfc18479fb8d496f8304ed7992
willwclo <willwclo@gmail.com>
2014-06-25 14:44:57 -0700

    Change user survey page

--- public/js/users/subroutes/account/controllers/UsersAccountCtrl.js
+++ public/js/users/subroutes/account/controllers/UsersAccountCtrl.js
@@ -1,16 +1,17 @@
-app.controller('UsersAccountCtrl', ['$scope','UsersAccount', 'UserTag', 
+app.controller('UsersAccountCtrl', ['$scope', 'UsersAccount', 'UserTag', 
 function ($scope, UsersAccount, UserTag) {
   $scope.pendingRequests = 0;
   $scope.submitText = 'Save Your Profile';
+
   UsersAccount.get().then(function (user) {
     $scope.user = user;
     $scope.binary = user.tags.filter(function (item) { return item.tag.type === 'binary'; });
     $scope.scale = user.tags.filter(function (item) { return item.tag.type === 'scale'; });
     $scope.text = user.tags.filter(function (item) { return item.tag.type === 'text'; });
 
+    $scope.labels = {};
     $scope.tags = {};
     user.tags.forEach(function (item) {
-
       // establish category
       var cat = item.tag.category.name;
       $scope.tags[cat] = $scope.tags[cat] || {};
@@ -19,8 +20,19 @@ function ($scope, UsersAccount, UserTag) {
       var type = item.tag.type;
       $scope.tags[cat][type] = $scope.tags[cat][type] || [];
       $scope.tags[cat][type].push(item);
+
+      // establish label
+      var label = item.tag.category.label;
+      $scope.labels[cat] = $scope.labels[cat] || label;
     });
   });
+
+  $scope.updateSearchStage = function (value) {
+    $scope.user.searchStage = value;
+  };
+  $scope.isSearchStage = function (value) {
+    return $scope.user.searchStage === value;
+  };
 
   $scope.update = function () {
     // re-compile tags
