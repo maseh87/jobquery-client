e4e2d687fc3ef933af8833e4cca8b2e208e93e08
Evan Kim <evan.kim.390@gmail.com>
2014-06-26 14:11:43 -0700

    UX improvements for password reset pages

--- public/js/core/controllers/SendCtrl.js
+++ public/js/core/controllers/SendCtrl.js
@@ -4,7 +4,11 @@ function($scope, AuthService) {
   $scope.sendPasswordReset = function (email) {
     $scope.submitting = true;
     AuthService.sendPasswordReset(email).then(function(response){
-      console.log(response);
+      $scope.success = true;
+      $scope.submitting = false;
+    }, function(response){
+      $scope.success = true;
+      $scope.submitting = false;
     });
   }
 
