f78e93a23fbef6852ca7146bd8f66baa400580fd
Conor Fennell <fennell.conor@gmail.com>
2014-06-25 22:09:10 -0700

    One candidate per time slot constraint

--- public/js/admin/subroutes/matches/services/SchedulerServ.js
+++ public/js/admin/subroutes/matches/services/SchedulerServ.js
@@ -55,10 +55,12 @@ app.factory('Scheduler', ['Opportunity', 'User', '$q', function(Opportunity, Use
   };
 
   var filterActiveOpportunities = function (opportunities) {
-    return opportunities.filter(function (opportunity) {
+    opportunities = opportunities.filter(function (opportunity) {
        if(!opportunity.active) return false;
        return true;
     });
+    //TODO REMOVE REMOVE HARDCODED oppertunities
+    return opportunities.slice(0,5);
   };
 
   var assignSlots = function (queuedSlots) {
@@ -90,19 +92,48 @@ app.factory('Scheduler', ['Opportunity', 'User', '$q', function(Opportunity, Use
   };
 
   var doPass = function (board, queuedSlots) {
-    for (var i = 0; i < queuedSlots.length; i++) {
-      for (var j = 0; j < queuedSlots[i].length; j++) {
-        console.log('pass',i,j, queuedSlots[i][j] );
-        if (board[i][j] === undefined) {
+    console.log('pass', board );
+    for (var x = 0; x < queuedSlots.length; x++) {
+      for (var y = 0; y < queuedSlots[x].length; y++) {
+        if (board[x][y] === undefined) {
           // assign slot
-          board[i][j] = queuedSlots[i][j].pop();
+          board[x][y] = queuedSlots[x][y].pop();
           // check constraints
+          if(!constraintsValid(board)) {
             // remove assignment
+            board[x][y] = undefined;
+          }
         }
       }
     }
   };
 
+var constraintsValid = function (board) {
+  var isValid = true;
+  // check candidate is not assigned twice in the same time slot
+  isValid = isValid && oneCandidatePerTimeSlot(board);
+  // check candidate is not assigned twice to interview with the same oppertunity
+  return isValid;
+};
+
+var oneCandidatePerTimeSlot = function (board) {
+  var valid = true;
+  var unique;
+  for (var y = 0; y < board[0].length; y++) {
+    unique = {};
+    for (var x = 0; x < board.length; x++) {
+      console.log(unique);
+      if (board[x][y] !== undefined) {
+        unique[board[x][y]] = unique[board[x][y]] === undefined ? 0 : unique[board[x][y]];
+        unique[board[x][y]]++;
+        if (unique[board[x][y]] > 1) {
+          valid = false;
+        }
+      }
+    }
+  }
+  return valid;
+};
 
   return {
     schedule : function(numberOfslots) {
