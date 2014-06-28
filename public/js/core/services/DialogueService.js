cf8c5df70d4f02cec291d1ac4405889a149f502c
willwclo <willwclo@gmail.com>
2014-06-28 14:45:09 -0700

    Update with show and hide methods

--- public/js/core/services/DialogueService.js
+++ public/js/core/services/DialogueService.js
@@ -10,7 +10,17 @@ app.factory('DialogueService', function () {
   };
 
   serviceMethods.toggle = function () {
-    serviceMethods._hidden = !serviceMethods._hidden;
+    serviceMethods._hidden ? serviceMethods.show() : serviceMethods.hide();
+  };
+
+  serviceMethods.show = function () {
+    serviceMethods._hidden = false;
+    document.getElementById('dialogue-box').setAttribute('display', 'block');
+  };
+
+  serviceMethods.hide = function () {
+    serviceMethods._hidden = false;
+    document.getElementById('dialogue-box').setAttribute('display', 'none');
   };
 
   return serviceMethods;
