b674276069398da895f20554a28c2d1af523989a
Evan Kim <evan.kim.390@gmail.com>
2014-06-27 22:26:46 -0700

    Change invite candidates to candidates all on sidebar

--- public/js/admin/subroutes/dashboard/state.js
+++ public/js/admin/subroutes/dashboard/state.js
@@ -2,7 +2,7 @@ app.config(['$stateProvider', function ($stateProvider) {
 
   $stateProvider
     .state('admin.dashboard', {
-      url: '/dashboard/:_id',
+      url: '/dashboard',
       templateUrl: '/js/admin/subroutes/dashboard/templates/dashboard.tpl.html',
       controller: 'AdminDashboardCtrl'
     });
