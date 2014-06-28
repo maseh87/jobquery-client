e739eed69bcc12467ebd25f4134753ae13f8b468
willwclo <willwclo@gmail.com>
2014-06-27 21:26:25 -0700

    Harmonize admin sidebar with user

--- public/js/users/controllers/UsersSidebarCtrl.js
+++ public/js/users/controllers/UsersSidebarCtrl.js
@@ -1,3 +1,32 @@
-app.controller('UsersSidebarCtrl', ['$scope', function ($scope) {
+app.controller('UsersSidebarCtrl', ['$scope', '$rootScope', '$state', function ($scope, $rootScope, $state) {
+
+    // ui-sref-active does not supported nested states right now, hence this ugly fix:
+    // https://github.com/angular-ui/ui-router/issues/704
+
+    var sidebarIds = {
+        'dashboard': 'sidebar-dashboard',
+        'opportunities': 'sidebar-opportunities',
+        'companies': 'sidebar-companies',
+        'account': 'sidebar-account',
+        'profile': 'sidebar-profile',
+        'candidates': 'sidebar-candidates',
+        'matches': 'sidebar-matches',
+        'tags': 'sidebar-tags',
+    };
+
+    $scope.currentStateHeading = "opportunities";
+
+    $rootScope.$on('$stateChangeSuccess', function(event, toState) {
+        var fromState = $scope.currentStateHeading;
+        document.getElementById(sidebarIds[fromState]).classList.remove('sidebar-active');
+
+        var newState = Object.keys(sidebarIds).filter(function (state) {
+            return toState.name.indexOf(state) > -1;
+        })[0];
+        if (newState) { 
+            $scope.currentStateHeading = newState;
+            document.getElementById(sidebarIds[newState]).classList.add('sidebar-active');
+        }
+    });
 
 }]);
