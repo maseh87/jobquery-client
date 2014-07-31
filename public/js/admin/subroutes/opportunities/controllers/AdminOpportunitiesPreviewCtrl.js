c51d77f5da8e3cad4fbb8a95a8b13ad27a226ad2
Mason <maseh87@gmail.com>
2014-07-31 16:33:52 -0700

    removed unneccessary code

--- public/js/admin/subroutes/opportunities/controllers/AdminOpportunitiesPreviewCtrl.js
+++ public/js/admin/subroutes/opportunities/controllers/AdminOpportunitiesPreviewCtrl.js
@@ -3,8 +3,6 @@ app.controller('AdminOpportunitiesPreviewCtrl', function($scope, $stateParams, O
   Opportunity.get($stateParams._id).then(function (data) {
     console.log(data);
     $scope.opportunity = data;
-    // var questions = opportunity.questions;
-    // $scope.opportunity = opportunity;
     $scope.company = data.company;
   });
 });
