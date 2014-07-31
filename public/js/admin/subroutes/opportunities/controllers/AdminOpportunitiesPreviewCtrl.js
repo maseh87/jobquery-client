d5674f11d12ebc4d45c969150e8ad297121a157e
Mason <maseh87@gmail.com>
2014-07-31 16:11:21 -0700

    retrieving correct opportunity info from the server

--- public/js/admin/subroutes/opportunities/controllers/AdminOpportunitiesPreviewCtrl.js
+++ public/js/admin/subroutes/opportunities/controllers/AdminOpportunitiesPreviewCtrl.js
@@ -7,5 +7,8 @@ app.controller('AdminOpportunitiesPreviewCtrl', function($scope, $stateParams, M
   // };
   Match.getUsers($stateParams._id).then(function (data) {
     console.log(data.opportunity);
+    var opportunity = data.opportunity;
+    var questions = opportunity.questions;
+    $scope.opportunity = opportunity;
   });
 });
