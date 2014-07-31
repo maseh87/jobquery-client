5767397b7178a4b8071bf7cbaef2d98de245640e
Mason <maseh87@gmail.com>
2014-07-31 16:15:09 -0700

    added company property need to retrieve the photo

--- public/js/admin/subroutes/opportunities/controllers/AdminOpportunitiesPreviewCtrl.js
+++ public/js/admin/subroutes/opportunities/controllers/AdminOpportunitiesPreviewCtrl.js
@@ -1,14 +1,10 @@
 app.controller('AdminOpportunitiesPreviewCtrl', function($scope, $stateParams, Match) {
-  // $scope.opportunity = {
-  //   jobTitle: 'Job Title',
-  //   company: {
-  //     name: 'Company Name'
-  //   }
-  // };
+
   Match.getUsers($stateParams._id).then(function (data) {
     console.log(data.opportunity);
     var opportunity = data.opportunity;
     var questions = opportunity.questions;
     $scope.opportunity = opportunity;
+    $scope.company = opportunity.company;
   });
 });
