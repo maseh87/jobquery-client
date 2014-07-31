2da4c9203d5b4fc2bc03e0d1493c37ce71733017
Mason <maseh87@gmail.com>
2014-07-31 16:55:52 -0700

    preview page fully populated and can be updated

--- public/js/admin/subroutes/opportunities/controllers/AdminOpportunitiesPreviewCtrl.js
+++ public/js/admin/subroutes/opportunities/controllers/AdminOpportunitiesPreviewCtrl.js
@@ -1,7 +1,7 @@
 app.controller('AdminOpportunitiesPreviewCtrl', function($scope, $stateParams, Opportunity) {
 
   Opportunity.get($stateParams._id).then(function (data) {
-    console.log(data);
+    // console.log(data.company);
     $scope.opportunity = data;
     $scope.company = data.company;
   });
