37ebd36f08c9b229990a04a462619ceaae7b842b
Mason <maseh87@gmail.com>
2014-07-31 16:04:25 -0700

    retrieving opportunity from server

--- public/js/admin/subroutes/opportunities/controllers/AdminOpportunitiesPreviewCtrl.js
+++ public/js/admin/subroutes/opportunities/controllers/AdminOpportunitiesPreviewCtrl.js
@@ -1,8 +1,11 @@
-app.controller('AdminOpportunitiesPreviewCtrl', function($scope) {
-  $scope.opportunity = {
-    jobTitle: 'Job Title',
-    company: {
-      name: 'Company Name'
-    }
-  };
+app.controller('AdminOpportunitiesPreviewCtrl', function($scope, $stateParams, Match) {
+  // $scope.opportunity = {
+  //   jobTitle: 'Job Title',
+  //   company: {
+  //     name: 'Company Name'
+  //   }
+  // };
+  Match.getUsers($stateParams._id).then(function (data) {
+    console.log(data.opportunity);
+  });
 });
