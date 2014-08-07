80f9b16203b2d89e5765ba6a61080c160ae9c23b
Mason <maseh87@gmail.com>
2014-08-06 19:32:11 -0700

    added opportunities and location to company view

--- public/js/users/subroutes/companies/controllers/UsersCompaniesCtrl.js
+++ public/js/users/subroutes/companies/controllers/UsersCompaniesCtrl.js
@@ -1,7 +1,17 @@
 app.controller('UsersCompaniesCtrl', ['$scope', 'UsersCompany', function ($scope, UsersCompany) {
-
+  $scope.opp = "Opportunities";
   UsersCompany.getAll().then(function (companies) {
     $scope.companies = companies;
+    console.log($scope.companies);
   });
+  $scope.num = function(index) {
+    if($scope.companies[index].opportunities.length === 1) {
+      $scope.opp = "Opportunity";
+      return $scope.companies[index].opportunities.length;
+    } else {
+      $scope.opp = "Opportunities";
+      return $scope.companies[index].opportunities.length;
+    }
+  };
 
 }]);
