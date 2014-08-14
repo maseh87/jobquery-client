7f6be8fa71dd797d1c0478ba7fa94e97cbd61965
joshWyatt <joshuawyatt123@gmail.com>
2014-08-13 18:42:05 -0700

    More tests

--- test/unit/controllers/AdminControllers/AdminCandidatesCtrlSpec.js
+++ test/unit/controllers/AdminControllers/AdminCandidatesCtrlSpec.js
@@ -1,10 +1,18 @@
 'use strict';
 
 describe('Unit: AdminCandidatesCtrl', function(){
+  var ctrl, scope, User, Match, $httpBackend;
+
   // Load the module with MainController
   beforeEach(module('jobQuery'));
 
-  var ctrl, scope;
+  // Inject services used inside controller
+  beforeEach(inject(function($injector){
+    User = $injector.get('User');
+    Match = $injector.get('Match');
+    $httpBackend = $injector.get('$httpBackend');
+  }));
+
   // inject the $controller and $rootScope services
   // in the beforeEach block
   beforeEach(inject(function($controller, $rootScope) {
@@ -12,54 +20,152 @@ describe('Unit: AdminCandidatesCtrl', function(){
     scope = $rootScope.$new();
     // Create the controller
     ctrl = $controller('AdminCandidatesCtrl', {
-      $scope: scope
+      $scope: scope,
+      User: User,
+      Match: Match
     });
+
   }));
+
+  // afterEach(function(){
+
+  //   $httpBackend.verifyNoOutstandingExpectation();
+  //   $httpBackend.verifyNoOutstandingRequest();
+
+  // });
 
   describe('$scope properties', function(){
 
-    it('should have a query property', function(){
-      expect(scope.query).toBeDefined();
+    describe('query property', function(){
+
+      it('should have a query property', function(){
+        expect(scope.query).toBeDefined();
+      });
+
+      it('should be an empty string', function(){
+        expect(scope.query).toBe('');
+      });
+
     });
 
-    it('should have a config property', function(){
-      expect(scope.config).toBeDefined();
+    describe('config property', function(){
+
+      it('should have a config property', function(){
+        expect(scope.config).toBeDefined();
+      });
+
+      it('should be an object', function(){
+        expect(typeof scope.config).toBe('object');
+      });
+
+      it('should have an exclude property', function(){
+        expect(scope.config.exclude).toBeDefined();
+      });
     });
 
-    it('should have a sorter property', function(){
-      expect(scope.sorter).toBeDefined();
+    describe('sorter property', function(){
+
+      it('should have a sorter property', function(){
+        expect(scope.sorter).toBeDefined();
+      });
+
+      it('should equal "name"', function(){
+        expect(scope.sorter).toBe('name');
+      });
     });
 
-    it('should have a noPercentMessage property', function(){
-      expect(scope.noPercentMessage).toBeDefined();
+    describe('noPercentMessage property', function(){
+
+      it('should have a noPercentMessage property', function(){
+        expect(scope.noPercentMessage).toBeDefined();
+      });
+
+      it('should equal "n/a"', function(){
+        expect(scope.noPercentMessage).toBe('n/a');
+      });
     });
+
   });
 
   describe('$scope methods', function(){
 
-    it('should have a toggleAccepted method', function(){
-      expect(scope.toggleAccepted).toBeDefined();
-      expect(typeof scope.toggleAccepted).toBe('function');
+    describe('toggleAccepted method', function(){
+
+      it('should have a toggleAccepted method', function(){
+        expect(scope.toggleAccepted).toBeDefined();
+        expect(typeof scope.toggleAccepted).toBe('function');
+      });
+
+      it('should call $scope.excludeAccepted when passed a truthy argument', function(){
+        spyOn(scope, 'excludeAccepted');
+        scope.toggleAccepted(true);
+        expect(scope.excludeAccepted).toHaveBeenCalled();
+      });
+
+      it('should call $scope.includeAccepted when passed a falsy argument', function(){
+        spyOn(scope, 'includeAccepted');
+        scope.toggleAccepted(false);
+        expect(scope.includeAccepted).toHaveBeenCalled();
+      });
+
     });
 
-    it('should have a excludeAccepted method', function(){
-      expect(scope.excludeAccepted).toBeDefined();
-      expect(typeof scope.excludeAccepted).toBe('function');
+    describe('excludeAccepted method', function(){
+
+      it('should have a excludeAccepted method', function(){
+        expect(scope.excludeAccepted).toBeDefined();
+        expect(typeof scope.excludeAccepted).toBe('function');
+      });
+
+      it('should create $scope.groups object', function(){
+        expect(scope.groups).toBeUndefined();
+
+        scope.excludeAccepted();
+
+        expect(scope.groups).toBeDefined();
+        expect(typeof scope.groups).toBe('object');
+
+      });
+
     });
 
-    it('should have a includeAccepted method', function(){
-      expect(scope.includeAccepted).toBeDefined();
-      expect(typeof scope.includeAccepted).toBe('function');
+    describe('includeAccepted method', function(){
+
+      it('should have a includeAccepted method', function(){
+        expect(scope.includeAccepted).toBeDefined();
+        expect(typeof scope.includeAccepted).toBe('function');
+      });
+
     });
 
-    it('should have a toggleCheckbox method', function(){
-      expect(scope.toggleCheckbox).toBeDefined();
-      expect(typeof scope.toggleCheckbox).toBe('function');
+    describe('toggleCheckbox method', function(){
+
+      it('should have a toggleCheckbox method', function(){
+        expect(scope.toggleCheckbox).toBeDefined();
+        expect(typeof scope.toggleCheckbox).toBe('function');
+      });
+
+      it('should call User.update', function(){
+        var user = {};
+        user._id = 2;
+        var property = true;
+        user.property = property;
+
+        spyOn(User, 'update');
+        scope.toggleCheckbox(user, property);
+        expect(User.update).toHaveBeenCalled();
+
+      });
+
     });
 
-    it('should have a downloadData method', function(){
-      expect(scope.downloadData).toBeDefined();
-      expect(typeof scope.downloadData).toBe('function');
+    describe('downloadData method', function(){
+
+      it('should have a downloadData method', function(){
+        expect(scope.downloadData).toBeDefined();
+        expect(typeof scope.downloadData).toBe('function');
+      });
+
     });
 
   });
