d46e38099707ca71ac52774d803436f42845f335
joshWyatt <joshuawyatt123@gmail.com>
2014-08-13 22:17:10 -0700

    More detailed tests

--- test/unit/controllers/AdminControllers/AdminCompaniesDetailCtrlSpec.js
+++ test/unit/controllers/AdminControllers/AdminCompaniesDetailCtrlSpec.js
@@ -7,7 +7,9 @@ describe('Unit: AdminCompaniesDetailCtrl', function(){
   var ctrl, scope;
   // inject the $controller and $rootScope services
   // in the beforeEach block
-  beforeEach(inject(function($controller, $rootScope) {
+  beforeEach(inject(function($controller, $rootScope, $injector) {
+
+
     // Create a new scope that's a child of the $rootScope
     scope = $rootScope.$new();
     // Create the controller
@@ -24,34 +26,58 @@ describe('Unit: AdminCompaniesDetailCtrl', function(){
 
   describe('$scope methods', function(){
 
-    it('should have a toggleEdit method', function(){
-      expect(scope.toggleEdit).toBeDefined();
-      expect(typeof scope.toggleEdit).toBe('function');
+    describe('$scope.toggleEdit', function(){
+
+      it('should have a toggleEdit method', function(){
+        expect(scope.toggleEdit).toBeDefined();
+        expect(typeof scope.toggleEdit).toBe('function');
+      });
+
     });
 
-    it('should have a update method', function(){
-      expect(scope.update).toBeDefined();
-      expect(typeof scope.update).toBe('function');
+    describe('$scope.update', function(){
+
+      it('should have an update method', function(){
+        expect(scope.update).toBeDefined();
+        expect(typeof scope.update).toBe('function');
+      });
+
     });
 
-    it('should have a addMedia method', function(){
-      expect(scope.addMedia).toBeDefined();
-      expect(typeof scope.addMedia).toBe('function');
+    describe('$scope.addMedia', function(){
+
+      it('should have an addMedia method', function(){
+        expect(scope.addMedia).toBeDefined();
+        expect(typeof scope.addMedia).toBe('function');
+      });
+
     });
 
-    it('should have a removeMedia method', function(){
-      expect(scope.removeMedia).toBeDefined();
-      expect(typeof scope.removeMedia).toBe('function');
+    describe('$scope.removeMedia', function(){
+
+      it('should have a removeMedia method', function(){
+        expect(scope.removeMedia).toBeDefined();
+        expect(typeof scope.removeMedia).toBe('function');
+      });
+
     });
 
-    it('should have a addLink method', function(){
-      expect(scope.addLink).toBeDefined();
-      expect(typeof scope.addLink).toBe('function');
+    describe('$scope.addLink', function(){
+
+      it('should have an addLink method', function(){
+        expect(scope.addLink).toBeDefined();
+        expect(typeof scope.addLink).toBe('function');
+      });
+
     });
 
-    it('should have a removeLink method', function(){
-      expect(scope.removeLink).toBeDefined();
-      expect(typeof scope.removeLink).toBe('function');
+    describe('$scope.removeLink', function(){
+
+      it('should have a removeLink method', function(){
+        expect(scope.removeLink).toBeDefined();
+        expect(typeof scope.removeLink).toBe('function');
+      });
+
     });
 
   });
