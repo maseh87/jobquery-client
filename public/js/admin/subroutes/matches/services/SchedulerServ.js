cc11e0e6743b0255916623a72ccc9df6efcd283f
Xianhui Feng <xianhui.feng100@gmail.com>
2014-08-18 15:54:45 -0700

    Implemented scheduleAllMatches function

--- public/js/admin/subroutes/matches/services/SchedulerServ.js
+++ public/js/admin/subroutes/matches/services/SchedulerServ.js
@@ -23,32 +23,63 @@ var createScheduleMatrix = function(opportunities) {
   return scheduleMatrix;
 };
 
-var scheduleMatrix = createScheduleMatrix(opportunities);
+var scheduleMatrix;
 
 var makeScheduleData = function(usersForSchedule, opportunities, matchesSortedByInterest) {
   var opportunityAppointment;
-  console.dir(scheduleMatrix);
+  scheduleMatrix = createScheduleMatrix(opportunities);
+
   return opportunityAppointment;
 };
-
-
-//for everything interestLevel
-
-  //while matchesSortedByInterest at this interestLevel has keys
-
-    //for each interestLevel starting at the lowest
-      //for each userId
-        //if interestLevel is less than 11
-          //if # for this user equals numberOfRoundsScheduledTicker
+makeScheduleData(usersForSchedule, opportunities, matchesSortedByInterest);
+console.dir(scheduleMatrix);
+//////scheduleAllMatches()/////////////////
+var scheduleAllMatches =function() {
+  //for everything interestLevel
+  for(var interestLevel = 14; interestLevel > 1; interestLevel--){
+    var numberOfRoundsScheduledTicker = 0;
+    //while matchesSortedByInterest at this interestLevel has keys
+    var matchesForThisInterestLevel = matchesSortedByInterest[interestLevel];
+    while ( matchesForThisInterestLevel ) {
+      //for each interestLevel starting at the lowest
+      for(var numberOfRequests in matchesForThisInterestLevel){
+        //for each userId
+        for(var userId in matchesForThisInterestLevel[numberOfRequests]){
+          //if interestLevel is less than 11
+          if( interestLevel < 11 ){
+            //if # for this user equals numberOfRoundsScheduledTicker
+            if(usersForSchedule[userId].numberOfRounds === numberOfRoundsScheduledTicker) {
+              //pop oppId and schedule it(schedule it is a helper function)
+              var oppToSchedule = matchesForThisInterestLevel[numberOfRequests][userId].pop();
+              //scheduleSingleOpp(oppToSchedule, userId);
+              console.log("Calling scheduleSingleOpp()");
+            }
+          }else{
             //pop oppId and schedule it(schedule it is a helper function)
-        //else
-          //pop oppId and schedule it(schedule it is a helper function)
+            var oppToSchedule = matchesForThisInterestLevel[numberOfRequests][userId].pop();
+            //scheduleSingleOpp(oppToSchedule, userId);
+            console.log("Calling scheduleSingleOpp()");
+          }
 
-        //check if userId's value is empty
-          //delete userId
-
-      //if the interestLevel has no properties, delete it
-
+          //check if userId's value is empty
+          if(matchesForThisInterestLevel[numberOfRequests][userId].length === 0){
+            //delete userId
+            delete matchesForThisInterestLevel[numberOfRequests][userId];
+          }
+        }
+        //if the numberOfRequests has no properties, delete it
+        if( Object.keys(matchesForThisInterestLevel[numberOfRequests]).length === 0 ) {
+          delete matchesForThisInterestLevel[numberOfRequests];
+        }
+      }
+      numberOfRoundsScheduledTicker++;
+      //if the matchesForThisInterestLevel has no properties, delete it
+      if( Object.keys(matchesForThisInterestLevel).length === 0 ) {
+        delete matchesForThisInterestLevel;
+      }
+    }
+  }
+};
 
 /////scheduler function//////
 // inputs oppId, userId
@@ -102,7 +133,7 @@ var makeScheduleData = function(usersForSchedule, opportunities, matchesSortedBy
 
 
 
-makeScheduleData(usersForSchedule, opportunities, matchesSortedByInterest);
+
 
 
 
