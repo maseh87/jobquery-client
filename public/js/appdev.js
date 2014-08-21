d36b0602c2f76846d2cd64f916d4c5baa1642b2e
Mason <maseh87@gmail.com>
2014-08-21 00:53:01 -0700

    added glyphicon on loading

--- public/js/appdev.js
+++ public/js/appdev.js
@@ -7,4 +7,29 @@ app.config(['$httpProvider', function($httpProvider) {
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
   // Remove the header used to identify ajax call that would prevent CORS from working
   // http://thibaultdenizet.com/tutorial/cors-with-angular-js-and-sinatra/
+  $httpProvider.interceptors.push('LoadingInterceptor');
+
+}]);
+
+app.run(function($rootScope) {
+  $rootScope.notifications = {
+    loading: false
+  };
+
+});
+
+app.factory('LoadingInterceptor', ['$rootScope', function($rootScope) {
+  return {
+    response: function(obj) {
+      console.log('response');
+      $rootScope.notifications.loading = false;
+      return obj;
+    },
+    request: function(obj) {
+      console.log('request');
+
+      $rootScope.notifications.loading = true;
+      return obj;
+    }
+  };
 }]);
