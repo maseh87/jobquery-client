9405d29bae23c6ae0f2321a21ef4aa49b820a011
Mason <maseh87@gmail.com>
2014-08-23 17:47:54 -0700

    added Oxford for chart directive

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('jobQuery', ['ui.router', 'ngResource', 'LocalStorageModule', 'ui.bootstrap', 'ui.bootstrap.tpls', 'videosharing-embed', 'ngAnimate']);
+var app = angular.module('jobQuery', ['ui.router', 'ngResource', 'LocalStorageModule', 'ui.bootstrap', 'ui.bootstrap.tpls', 'videosharing-embed', 'ngAnimate', 'oxford']);
 
 app.constant('SERVER_URL', '/* @echo SERVER_URL */');
 
